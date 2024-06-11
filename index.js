d9052cf09c9070a255b174da9c167a465313597e
KrishDevCrafting <krishkumer150@gmail.com>
2024-06-12 00:09:50 +0530

    kerosence

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello bhai kaise hai");
+const express = require('express')
+const app = express()
+const port = 7000
+
+app.get('/', (req, res) => {
+  res.send('Kerosence!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
