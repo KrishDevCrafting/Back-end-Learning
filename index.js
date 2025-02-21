6fa9ed2b1d8b269e0bbd931c55f2a5d2176e921c
Krish <krishkumer150@gmail.com>
2025-02-21 14:23:24 +0530

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 2000
+const express = require("express");
+const app = express();
+const port = 2000;
 
-app.get('/', (req, res) => {
-  res.send('Kerosence!')
-})
+app.get("/", (req, res) => {
+  res.send("Kerosence!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
