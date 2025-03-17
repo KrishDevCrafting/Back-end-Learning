b153a1f806850f24aeb1a1b7e5050ed7c25e3ca0
Krish <krishkumer150@gmail.com>
2025-03-17 15:13:47 +0530

    Updated

--- controller/user.js
+++ controller/user.js
@@ -1,19 +1,28 @@
 const User = require("../models/userSchema.js");
 
 const createUser = async (req, res) => {
-    const { name, email, password , gender } = req.body;
-    try {
-      const user = new User({ name, email, password, gender });
-      await user.save();
-      res.status(201).send({
-        message: "User created successfully",
-        user,
-      });
-    } catch (error) {
-      res.status(400).send({
-        message: "error creating user",
-        error,
-      });
-    }
-  };
-  module.exports = { createUser };
+  const { name, email, password, gender } = req.body;
+  try {
+    const user = new User({ name, email, password, gender });
+    await user.save();
+    res.status(201).send({
+      message: "User created successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(400).send({
+      message: "error creating user",
+      error,
+    });
+  }
+};
+const getusers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createUser, getusers };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUser, logoutUser } = require("../controller/user");
+const {
+  createUser,
+  loginUser,
+  logoutUser,
+  getusers,
+} = require("../controller/user");
 
 router.post("/register", createUser);
+router.get("/getusers", getusers);
 // router.post("/login", loginUser);
 // router.post("/logout", logoutUser);
 
